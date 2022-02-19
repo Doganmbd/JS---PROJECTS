0eefa9770efaeb0db56aad20644d67359fc95495
DMB29 <mbilaldogan90@gmail.com>
2022-02-19 11:07:40 +0300

    finish

--- basic-example-3 - Basic calculator/script.js
+++ basic-example-3 - Basic calculator/script.js
@@ -1,41 +1,40 @@
 let explain = document.getElementById("error");
 let calculate = document.getElementById("calculater");
+let numOne = document.getElementById("num1") ;  // istersem global de value suz tanımlarım ve içeride işlemlerimi Number(numOne.value) şeklinde gerçekleştirirm.
+
+
 
 // butona tıkladığıım zaman ;
 calculate.addEventListener("click" , () => {
     // input değerleri value alınır. num1 ve num2 sayıya çevirmek için Number kullandım.
-    let numOne = Number(document.getElementById("num1").value) ;
     let numTwo = Number(document.getElementById("num2").value);
     let optr = document.getElementById("operator").value;
     let result = 0 ;
-    // şayet sayı girilmezse ;
-    if (isNaN(numOne) || isNaN(numTwo)) {
+    // şayet sayı girilmezse ; Ama direk HTML de number yaparak kullanıcının sadece rakam girmesini sağladım
+/*     if (isNaN(Number(numOne.value)) || isNaN(numTwo)) {
         explain.innerText ="Please enter a number !" ;
+            else{
+       
     }
-
+ */
     // switch-case kullanmalıyız . ifadenin deeri her adımda değerlndidrilirbir eşleşme varsa ilgili kod bloğu döner. eşleşme yoksa default değer döner. default değer olmasa hepsine explain.innerHTML = result ;yazmaya gerek yok sadece en alta yazarız.
     
-    else{
-        
+
         switch(optr) {
             case "+" :
-                result = numOne + numTwo
-                explain.innerHTML = result ;
+                explain.innerHTML = Number(numOne.value) + numTwo
                 break ;
 
             case "-" :
-                result = numOne - numTwo
-                explain.innerHTML = result ;
+                explain.innerHTML = Number(numOne.value)  - numTwo
                 break ;
                 
             case "*" :
-                result = numOne * numTwo 
-                explain.innerHTML = result ;  
+                explain.innerHTML = Number(numOne.value) * numTwo 
                 break ; 
                 
             case "/" :
-                result = numOne / numTwo
-                explain.innerHTML = result ;
+                explain.innerHTML = Number(numOne.value) / numTwo
                 break ;
 
             default :
@@ -46,5 +45,5 @@ calculate.addEventListener("click" , () => {
     }
     
     
-})
+)
 
