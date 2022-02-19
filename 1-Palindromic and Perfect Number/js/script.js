2ce223dcedba66c4fc6b0353496e5987d8955d23
DMB29 <mbilaldogan90@gmail.com>
2022-02-19 11:14:38 +0300

    finish

--- 1-Palindromic and Perfect Number/js/script.js
+++ 1-Palindromic and Perfect Number/js/script.js
@@ -1,8 +1,8 @@
 // toString() metodu, kendisine tanımlanan değişken değerini string olarak değiştirerek döndürür.
 
-let PalindromInput = document.querySelector(".pali") ;
-let PalindromButton = document.querySelector(".pali1") ;
-let PalindromTextarea = document.querySelector(".pali2") ;
+let PalindromInput = document.querySelector("#pali3") ;
+let PalindromButton = document.querySelector("#pali1") ;
+let PalindromTextarea = document.querySelector("#pali2") ;
 
 PalindromButton.addEventListener("click" , () =>{
     for (let index = 0; index < PalindromInput.value; index++) {
@@ -16,50 +16,38 @@ PalindromButton.addEventListener("click" , () =>{
 
 // perfect number
 
-/* let perfectInput = document.querySelector(".per1") ;
+let perfectInput = document.querySelector(".per1") ;
 let perfectButton = document.querySelector(".per2") ;
 let perfectTextarea = document.querySelector(".per3") ;
 
 
-
-perfectButton.addEventListener("click", () => { let sum = 0
-    for (let i = 1; i < perfectInput.value; i++) {
-        if(perfectInput.value % i == 0)
-
-        sum += i
-        if (sum == i ) {
-            perfectTextarea.value += i + "\n"
-            console.log(perfectTextarea.value);
-        }
+console.log(perfectTextarea);
+perfectButton.addEventListener("click", () => { 
+    if (perfectTextarea.value) {
+        perfectTextarea.value =""
         
+    }
+    for (let i = 1; i <= perfectInput.value; i++) {
+        let sum = 0
+        for(let j=1 ; j<=i/2 ; j++){
+            if(i % j == 0){
+                sum += j
+            }
         }
-        console.log(sum);
-
-
-
-}) */
-
-
-function find_perfect_number(num) {
-
-    let arr = [];
-    let sum = 0;
-    let arrSum = 0;
-    for (let i = 0; i < num; i++) {
-        if (num % i === 0) {
-            arr.push(i);
-            sum += i;
+        if(sum == i){
+            perfectTextarea.value += i + "\n"
         }
     }
-    arr.push(num);
-    for (let i = 0; i < arr.length; i++) {
-        arrSum += arr[i];
+})
+
+perfectInput.addEventListener("click", () => { 
+    if(perfectInput.value){      // perfectInput.value doluysa yani true'dur. içini boş yap
+        perfectTextarea.value =""
+        perfectInput.value =""
     }
-    let avg = arrSum / 2;
 
-    if (sum === num && avg === num) console.log(num)
-}
+})
 
-for (let i = 1; i < 100; i++) {
-    find_perfect_number(i);
-}
+
+
+
